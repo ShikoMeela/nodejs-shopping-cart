86508628c9aa26c426551184ec3c7b0932989cc0
Cecille Del Rosario <cecdelr@gmail.com>
2016-10-06 21:26:38 -0700

    Added sign-in

--- config/passport.js
+++ config/passport.js
@@ -56,3 +56,36 @@ passport.use('local.signup', new LocalStrategy({
 		});
 }));
 
+passport.use('local.signin', new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: true
+}, function(req, email, password, done){
+		req.assert('email', 'Invalid email').notEmpty().isEmail();
+		req.assert('password', 'Invalid password').notEmpty();
+
+		var errors = req.validationErrors();
+
+		if(errors){
+			var messages = [];
+			errors.forEach(function(error){
+				messages.push(error.msg);
+			});
+			return done(null, false, req.flash('error', messages));
+		}
+		//find user
+		User.findOne({'email': email}, function(err, user){
+			if(err){
+				return done(err);
+			}
+
+			if(!user){
+				return done(null, false, {message: 'No user found.'});
+			}
+			if(!user.validPassword(password)){
+				return done(null, false, {message: 'Wrong password.'});	
+			}
+			return done(null, user);
+		});
+
+}));
